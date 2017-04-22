f26c76c1d02b812e61c189a7263bfcdea6a66eee
brownie-etsu <brownie@etsu.edu>
2017-04-21 20:57:57 -0400

    WIP 1

---
+++ CharacterCreatorWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace D20CharCreator
+{
+    /// <summary>
+    /// Interaction logic for CharacterCreatorWindow.xaml
+    /// </summary>
+    public partial class CharacterCreatorWindow : Window
+    {
+        Page backgroundPage;
+        Page classPage;
+
+        public CharacterCreatorWindow()
+        {
+            InitializeComponent();
+            LoadPages();
+        }
+
+        private void LoadPages()
+        {
+            backgroundPage = new BackgroundPage();
+            classPage = new ClassPage();
+            frame.Navigate(backgroundPage);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Owner.Show();
+        }
+
+        private void ClassButton_Click(object sender, RoutedEventArgs e)
+        {
+            frame.Navigate(classPage);
+        }
+
+        private void BackgroundButton_Click(object sender, RoutedEventArgs e)
+        {
+            frame.Navigate(backgroundPage);
+        }
+    }
+}
---
+++ LoginWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace D20CharCreator
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateAccountButton_Click(object sender, RoutedEventArgs e)
+        {
+            CreateAccountWindow accountWindow = new CreateAccountWindow();
+            accountWindow.Owner = this;
+            accountWindow.Show();
+            Hide();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            Hide();
+
+            if (Database.LogIn(UsernameInputBox.Text, PasswordInputBox.Password))
+            {
+                CharacterListWindow charListWindow = new CharacterListWindow();
+                charListWindow.Show();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username/password.\nPlease try again.", "Login Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                Show();
+            }
+        }
+    }
+}
