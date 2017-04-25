bef2b5f8f483878a570ade8e7c49629307eed517
Nicholas <yoderna@etsu.edu>
2017-04-24 22:09:45 -0400

    Add files via upload

---
+++ D20/CharCreatorTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using D20CharCreator;
+
+namespace D20CharCreatorTest
+{
+    [TestClass]
+    public class CharCreatorTest
+    {
+        [TestMethod]
+        public void CreateAccountWithValidInformation()
+        {
+        }
+
+        [TestMethod]
+        public void CreateAccountWithInvalidInformation()
+        {
+        }
+
+        [TestMethod]
+        public void LoginReturnsTrueWhenUsernameAndPasswordAreCorrect()
+        {
+        }
+
+        [TestMethod]
+        public void LoginReturnsFalseWhenUsernameAndPasswordAreIncorrect()
+        {
+        }
+
+        [TestMethod]
+        public void viewcharactergood()
+        {
+        }
+
+        [TestMethod]
+        public void viewcharacterbad()
+        {
+        }
+
+        [TestMethod]
+        public void backgroundgood()
+        {
+        }
+
+        [TestMethod]
+        public void backgroundbad()
+        {
+        }
+
+        [TestMethod]
+        public void classgood()
+        {
+        }
+
+        [TestMethod]
+        public void classbad()
+        {
+        }
+    }
+}
