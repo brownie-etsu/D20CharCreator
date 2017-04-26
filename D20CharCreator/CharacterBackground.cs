d2a4eb229234ef34a15c4c1446dad0ebb45f225a
Nicholas <yoderna@etsu.edu>
2017-04-26 02:25:35 -0400

    Add files via upload

--- D20CharCreator/CharacterBackground.cs
+++ D20CharCreator/CharacterBackground.cs
@@ -6,8 +6,18 @@ using System.Threading.Tasks;
 
 namespace D20CharCreator
 {
-    public enum CharacterBackground
+    public class CharacterBackground
     {
+        public BackgroundType Type { get; set; }
 
+        public Language LangOne { get; set; }
+
+        public Language LangTwo { get; set; }
+
+        public int Equipment { get; set; }
+
+        public int[] Characteristics { get; set; }
+
+        public int Rerolls { get; set; }
     }
 }
--- D20CharCreator/CharacterListWindow.xaml.cs
+++ D20CharCreator/CharacterListWindow.xaml.cs
@@ -22,30 +22,64 @@ namespace D20CharCreator
     /// </summary>
     public partial class CharacterListWindow : Window
     {
-        private List<Character> _characters = new List<Character>();
+        private int _userId;
+        private List<Character> _characters;
 
         public CharacterListWindow()
         {
             InitializeComponent();
         }
 
+        public CharacterListWindow(int userId)
+        {
+            _userId = userId;
+            InitializeComponent();
+        }
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            List<object> stuff = new List<object>();
-            var a = new { Class = ClassType.BARBARIAN, Name = "Name 1" };
-            var b = new { Class = ClassType.ROGUE, Name = "Name 2" };
-            stuff.Add(a);
-            stuff.Add(b);
+            _characters = new List<Character>(Database.GetCharacterList(_userId));
 
-            CharListTable.ItemsSource = stuff;
-
-            Database.GetCharacterList(17);
+            CharListTable.ItemsSource = _characters;
         }
 
         private void AddCharacterButton_Click(object sender, RoutedEventArgs e)
         {
             Hide();
-            CharacterCreatorWindow charCreatorWin = new CharacterCreatorWindow();
+
+            Character newCharacter = new Character();
+
+            int id = Database.CreateCharacter(newCharacter, _userId);
+            newCharacter.CharacterId = id;
+            newCharacter.Name = "Character " + id;
+
+            _characters.Add(newCharacter);
+            CharListTable.ItemsSource = null;
+            CharListTable.ItemsSource = _characters;
+
+            CharacterCreatorWindow charCreatorWin = new CharacterCreatorWindow(newCharacter);
+            charCreatorWin.Owner = this;
+            charCreatorWin.Show();
+        }
+
+        public void DeleteCharacterButton_Click(object sender, RoutedEventArgs e)
+        {
+            Character charToDelete = ((FrameworkElement)sender).DataContext as Character;
+
+            Database.DeleteCharacter(charToDelete);
+
+            _characters.Remove(charToDelete);
+
+            CharListTable.Items.Refresh();
+        }
+
+        public void EditCharacterButton_Click(object sender, RoutedEventArgs e)
+        {
+            Hide();
+
+            Character charToEdit = ((FrameworkElement)sender).DataContext as Character;
+
+            CharacterCreatorWindow charCreatorWin = new CharacterCreatorWindow(charToEdit);
             charCreatorWin.Owner = this;
             charCreatorWin.Show();
         }
